1a9298c28c8829af3e569a053910bc2bf44235d0
tu2-atmanand <sanketgauns8@gmail.com>
2023-08-10 14:17:23 +0530

    first commit

---
+++ FrontEnd/src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="Home">
+      <div className="container">
+
+      </div>
+    </div>
+  );
+}
+
+export default Home;
