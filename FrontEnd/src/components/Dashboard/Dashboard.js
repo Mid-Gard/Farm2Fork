c16324d60420b9c5d8cce5cfeb087fad1c280883
tu2-atmanand <sanketgauns8@gmail.com>
2023-08-14 12:58:49 +0530

    Setup properly from Mid-Gard

--- FrontEnd/src/components/Dashboard/Dashboard.js
+++ FrontEnd/src/components/Dashboard/Dashboard.js
@@ -4,8 +4,69 @@ import "./Dashboard.css";
 function Dashboard() {
   return (
     <div className="Dashboard">
-      <div className="container">
-
+      <div className="containerr">
+        <div className="details">
+          <h1>Bevarages</h1>
+          <h2>Sold</h2>
+          <div className="detailsContent">
+            <p>Wheat : 10kg Validy : 2 months</p>
+            <p>Rice : 10kg Validy : 2 months</p>
+          </div>
+          <h2>Left</h2>
+          <div className="detailsContent">
+            <p>Wheat : 10kg Validy : 2 months</p>
+            <p>Rice : 10kg Validy : 2 months</p>
+            <p>Coffee : 10kg Validy : 2 months</p>
+          </div>
+        </div>
+        <div className="iot">
+          <h1>IOT Device Data</h1>
+          <h2>Foundary</h2>
+          <div className="detailsContent">
+            <p>Cows in farm : 50</p>
+            <p>Cows outside farm : 40</p>
+            <p>Prgnant Cows : 10</p>
+            <p>Unhealthy Cows : 3</p>
+          </div>
+          <h2>Field</h2>
+          <div className="detailsContent">
+            <p>Wheat temp : </p>
+            <p>Rice field </p>
+            <p>Coffee Field</p>
+          </div>
+        </div>
+      </div>
+      <div className="containerr">
+        <div className="details">
+          <h1>Bevarages</h1>
+          <h2>Sold</h2>
+          <div className="detailsContent">
+            <p>Wheat : 10kg Validy : 2 months</p>
+            <p>Rice : 10kg Validy : 2 months</p>
+          </div>
+          <h2>Left</h2>
+          <div className="detailsContent">
+            <p>Wheat : 10kg Validy : 2 months</p>
+            <p>Rice : 10kg Validy : 2 months</p>
+            <p>Coffee : 10kg Validy : 2 months</p>
+          </div>
+        </div>
+        <div className="iot">
+          <h1>IOT Device Data</h1>
+          <h2>Foundary</h2>
+          <div className="detailsContent">
+            <p>Cows in farm : 50</p>
+            <p>Cows outside farm : 40</p>
+            <p>Prgnant Cows : 10</p>
+            <p>Unhealthy Cows : 3</p>
+          </div>
+          <h2>Field</h2>
+          <div className="detailsContent">
+            <p>Wheat temp : </p>
+            <p>Rice field </p>
+            <p>Coffee Field</p>
+          </div>
+        </div>
       </div>
     </div>
   );
